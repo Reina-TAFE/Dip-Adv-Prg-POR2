0ec14d711d9563b35dafbb0dba6788ef1f19a638
20066312 <20066312@tafe.wa.edu.au>
2026-09-14 13:28:15 +0800

    feat: add classes PlayerBST and PayerBNode

---
+++ app/player_bnode.py
@@ -0,0 +1,27 @@
+class PlayerBNode:
+    def __init__(self, player):
+        self.__player = player
+        self.__subtree_left = None
+        self.__subtree_right = None
+
+    @property
+    def player(self):
+        return self.__player
+
+    @property
+    def subtree_left(self):
+        return self.__subtree_left
+
+    @subtree_left.setter
+    def subtree_left(self, value):
+        if value is not None and value != self.__subtree_left:
+            self.__subtree_left = value
+
+    @property
+    def subtree_right(self):
+        return self.__subtree_right
+
+    @subtree_right.setter
+    def subtree_right(self, value):
+        if value is not None and value != self.__subtree_right:
+            self.__subtree_right = value
---
+++ app/player_bst.py
@@ -0,0 +1,7 @@
+class PlayerBST:
+    def __init__(self):
+        self.__root = None
+
+    @property
+    def root(self):
+        return self.__root
